c2d59561633f1a46c65af6a200eea61a7ba25154
IZerab <beltramelorenzo1999@gmail.com>
2021-12-01 21:06:26 +0100

    Geopandas
    Added the plots that eliminates non localized posts.
    Plots the global distribution of the tweets.

---
+++ main.py
@@ -0,0 +1,32 @@
+# this is the main of the project
+
+# libraries
+import numpy as np
+import pandas as pd
+import geopandas
+import sklearn as sk
+import matplotlib.pyplot as plt
+
+# custom libraries
+from Preprocessing_functions import drop_non_geolocalised
+from Preprocessing_functions import localize_tweets
+
+# import the raw data
+data_donald = pd.read_csv("hashtag_donaldtrump.csv", lineterminator='\n')
+data_joe = pd.read_csv("hashtag_joebiden.csv", lineterminator='\n')
+
+# geolocalize Trump
+print("Donald Trump")
+data_donald = drop_non_geolocalised(data_donald, "lat", "long")
+
+# geolocalize Joe
+print("Joe Biden")
+data_joe = drop_non_geolocalised(data_joe, "lat", "long")
+
+# plotting Trump
+geo_donald = localize_tweets(data_donald, "World Trump data distribution")
+
+# plotting Biden
+geo_biden = localize_tweets(data_donald, "World Joe data distribution")
+
+
